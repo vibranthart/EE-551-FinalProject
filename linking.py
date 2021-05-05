cf728a4ac787187d9a904f7c481246e7c2cd4915
gaskin24 <39414998+gaskin24@users.noreply.github.com>
2021-05-05 19:28:53 -0400

    Update 2 linking.py
    fixed a bug, added reloading

--- linking.py
+++ linking.py
@@ -6,26 +6,38 @@ import pafy
 
 def StopVideoPlayback():
     media.stop()
+
+
 def StartVideoPlayback():
     media.play()
 
-#url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
-user_url = input("Please paste a url here: ")
 
-video = pafy.new(user_url)
+def PauseVideoPlayback():
+    media.pause()
 
-#best = video.streams[0]#gets index at 0
-best = video.getbest() #best quality start from beginning
+bool_end = True
+while bool_end:
+    bool_end = True
 
-media = vlc.MediaPlayer(best.url)
+    #url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+    user_url = input("Please paste a url here: ")
 
-print("Video Loaded!")
+    video = pafy.new(url=user_url, basic=True)
 
-start_playback = input("Would you like to end playback? (Y/N)")
-if ((start_playback == 'Y') or (start_playback == 'y')):
-    StartVideoPlayback()
+    # best = video.streams[0]#gets index at 0
+    best = video.getbest()  # best quality start from beginning
 
+    media = vlc.MediaPlayer(best.url)
 
-stop_playback = input("Would you like to end playback? (Y/N)")
-if ((stop_playback == 'Y') or (stop_playback == 'y')):
-    StopVideoPlayback()
+    print("Video Loaded!")
+
+    start_playback = input("Would you like to start playback? (Y/N)")
+    if ((start_playback == 'Y') or (start_playback == 'y')):
+        StartVideoPlayback()
+
+    stop_playback = input("Would you like to end playback? (Y/N)")
+    if ((stop_playback == 'Y') or (stop_playback == 'y')):
+        StopVideoPlayback()
+    reload_video = input("Would you like to play another video? (Y/N)")
+    if ((reload_video == 'N') or (reload_video == 'n')):
+        bool_end = False
