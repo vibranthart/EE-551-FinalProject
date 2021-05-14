32d0d1d53fbcc7deb1501a3f7f08fc79dd40b60e
gaskin24 <39414998+gaskin24@users.noreply.github.com>
2021-05-14 04:14:18 -0400

    Chat box working, pause and play buttons ready for commands

--- GUIscript.py
+++ GUIscript.py
@@ -1,20 +1,41 @@
 import tkinter as tk
 # Requires pip install requests and pip install Pillow (Pillow should already be installed for the vidReceiver and vidSender)
+#The GUI operates similar to div's in JS, order determines parents and child. (root is base window, any other child can be defined
+#  such as videoFrame is a child of root and controlsDiv is a child of videoFrame)
 HEIGHT = 720
 WIDTH = 1280
 global user_message
+chat_history = "\n"
 
+def chatHandler(message="init", outside_msg ="init"):
+    global chat_history
+    if (message=="init") and (outside_msg!= "init"):
+        chat_history += ("other: " + outside_msg + "\n")
+        chatHistory['text'] = str(chat_history)
+    elif (message!="init") and (outside_msg== "init"):
+        chat_history += ("u1: " + message + "\n")
+        chatHistory['text'] = str(chat_history)
+    elif ():
+        chat_history += ("u1: " + message + "\n")
+        chatHistory['text'] = str(chat_history)
+        chat_history += ("other: " + outside_msg + "\n")
+    
+    
 
 def messageHandler(text):
+    #HART, we can chat using this variable
     user_message = text
-    print("user_message")
+    chatHandler(user_message)
+    print(user_message)
 
 
 def pauseHandler():
+    #ADD call to pause video playback
     print("PAUSE CLICKED")
 
 
 def playHandler():
+    #ADD call to play video playback
     print("PLAY CLICKED")
 
 
@@ -36,21 +57,24 @@ controlsDiv = tk.Frame(videoFrame, bg='gray')
 controlsDiv.place(relheight=0.1, relwidth=1, rely=0.9, relx=0)
 
 pauseButton = tk.Button(controlsDiv, text='PAUSE',
-                        bg='blue', command=pauseHandler())
+                        bg='blue', command=lambda: pauseHandler())
 pauseButton.pack(side='left')
 playButton = tk.Button(controlsDiv, text='PLAY',
-                       bg='green', command=playHandler())
+                       bg='green', command=lambda: playHandler())
 playButton.pack(side='left')
 
 chatBox = tk.Frame(root, bg='#d9d9d9')
 chatBox.place(relx=0.6, rely=0.1, relwidth=0.3, relheight=0.8)
+
+chatHistory = tk.Label(chatBox, bg='#d9d9d9')
+chatHistory.place(relwidth=1, relheight=0.95)
 
 messageDiv = tk.Frame(chatBox, bg='gray')
 messageDiv.place(relwidth=1, relheight=0.05, relx=0, rely=0.95)
 
 textEntry = tk.Entry(messageDiv, bg='white')
 textEntry.pack(side='left')
-sendMessage = tk.Button(messageDiv, bg='red', text='SEND MESSAGE', command=Lambda: messageHandler(textEntry.get()))
+sendMessage = tk.Button(messageDiv, bg='red', text='SEND MESSAGE', command=lambda: messageHandler(textEntry.get()))
 sendMessage.pack(side='left')
 
 commandBox = tk.Frame(root, bg='#bfbfbf')
